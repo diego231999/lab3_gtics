1a05fc8c0fdadd201e4fa15ac9a781c43fbce004
JohamRV <a20181459@pucp.edu.pe>
2021-04-26 20:01:29 -0500

    job entity

---
+++ src/main/java/com/example/laboratorio3/entity/Jobs.java
@@ -0,0 +1,47 @@
+package com.example.laboratorio3.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "jobs")
+public class Jobs{
+
+    @Id
+    @Column(nullable = false)
+    private String jobId;
+    private String jobTittle;
+    private int minSalary;
+    private int maxSalary;
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getJobTittle() {
+        return jobTittle;
+    }
+
+    public void setJobTittle(String jobTittle) {
+        this.jobTittle = jobTittle;
+    }
+
+    public int getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(int minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public int getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(int maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+}
