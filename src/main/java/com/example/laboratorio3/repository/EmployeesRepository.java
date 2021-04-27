b336836d4dd8843cdce97bd71323777e22d3fa32
JohamRV <a20181459@pucp.edu.pe>
2021-04-26 20:18:31 -0500

    querys

--- src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -6,11 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
-    @Query(value="select e.first_name, e.last_name, h.start_date, h.end_date,job_title from employees e, jobs j, job_history h where e.job_id=j.job_id and h.job_id=j.job_id;", nativeQuery = true)
+    @Query(value="select e.first_name, e.last_name, h.start_date, h.end_date,job_title from employees e, jobs j, job_history h where e.job_id=j.job_id and h.job_id=j.job_id and e.salary>1500;", nativeQuery = true)
     Employees listaReportesEmployee();
 
+    @Query(value = "select e.first_name, e.last_name, j.job_title, d.department_name, e.hire_date   from employees e, jobs j, departments d where e.department_id = d.department_id and e.job_id = j.job_id order by e.first_name ASC;", nativeQuery = true)
+    List<Employees> historialEmployees;
+
+    List<Employees>  findByTerritorydescriptionContainingOrderByTerritorydescription (String text);
 
 }
