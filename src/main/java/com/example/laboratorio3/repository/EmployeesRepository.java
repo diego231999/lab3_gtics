1758ec0e63ec7415f3a6d11cf3dda6931fb3d3b3
Diego García <a20172367@pucp.edu.pe>
2021-04-26 19:45:33 -0500

    Cmabios en el repository y query

--- src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -1,12 +1,16 @@
 package com.example.laboratorio3.repository;
 
 
+import com.example.laboratorio3.entity.Employees;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface EmployeesRepository {
+public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
+    @Query(value="select e.first_name, e.last_name, h.start_date, h.end_date,job_title from employees e, jobs j, job_history h where e.job_id=j.job_id and h.job_id=j.job_id;", nativeQuery = true)
+    Employees listaReportesEmployee();
 
-//COMPLETAR
 
 }
