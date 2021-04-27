2c0b1e8c3f42982f67d3307bc0fbba990b6a7627
Victor Bolivar <victor.bolivar@pucp.edu.pe>
2021-04-26 19:47:16 -0500

    se añadio EmployeeRepository

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -1,34 +1,53 @@
 package com.example.laboratorio3.controller;
 
-//COMPLETAR
+import com.example.laboratorio3.entity.Employees;
+import com.example.laboratorio3.repository.EmployeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employee")
 public class EmployeeController {
 
-    //COMPLETAR
+    @Autowired
+    EmployeesRepository employeesRepository;
 
+    @GetMapping("/list")
     public String listaEmployee(   ){
-        //COMPLETAR
+        List<Employees> employeesList = employeesRepository.findAll();
+        for(Employees employee : employeesList){
+            System.out.println(employee.getFirstName());
+        }
+        return "employee/lista";
     }
 
-    public String nuevoEmployeeForm( ) {
-        //COMPLETAR
-    }
-
-
-    public String guardarEmployee() {
-        //COMPLETAR
-    }
-
-
-    public String editarEmployee() {
-        //COMPLETAR
-    }
-
-
-    public String borrarEmpleado() {
-
-       //COMPLETAR
-
-    }
+//    public String nuevoEmployeeForm( ) {
+//        //COMPLETAR
+//        return "";
+//    }
+//
+//
+//    public String guardarEmployee() {
+//        //COMPLETAR
+//        return "";
+//    }
+//
+//
+//    public String editarEmployee() {
+//        //COMPLETAR
+//        return "";
+//    }
+//
+//
+//    public String borrarEmpleado() {
+//
+//       //COMPLETAR
+//        return "";
+//    }
 
     //COMPLETAR
 
--- src/main/java/com/example/laboratorio3/entity/Employees.java
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -5,8 +5,40 @@ import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.*;
 import java.util.Date;
 
-//COMPLETAR
+@Entity
+@Table(name = "employees")
 public class Employees {
 
-           //COMPLETAR
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    //COMPLETAR relacion con otras tablas
 }
